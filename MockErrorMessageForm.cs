6b96c6f15d21ddff68b163dfd22356889bd97f21
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2019-09-18 14:52:46 -0700

    Add files via upload

--- MockErrorMessageForm.cs
+++ MockErrorMessageForm.cs
@@ -23,37 +23,55 @@ public class MockErrorMessageForm : Form
   private static Button leftButton = new Button();
   private static Button rightButton = new Button();
 
+  // This will be used in creating the graphic
+  private static Rectangle redCircle = new Rectangle();
+  private static Point[] whiteX;
+
   public MockErrorMessageForm() //this is the constructor
   {
-    // Set up the form/window
-    Text = "Warning";
+    // Set up texts
+    Text = "System Error";
+    text.Text = "Bro you just posted cringe!\nYou are going to lose subscriber!";
+    text.TextAlign = ContentAlignment.BottomCenter;
+    leftButton.Text = "OK";
+    rightButton.Text = "Accept";
+
+    // Set up sizes
     Height = 150;
     Width = Height*2;
+    text.AutoSize = true;
+    redCircle.Size = new Size(leftButton.Height*2, leftButton.Height*2);
 
-    // Initialize Controls
-    text.Text = "Bro you just posted cringe!\nYou are going to lose subscriber!";
-    text.Width = Width;
-    text.Top = text.Height;
-    text.TextAlign = ContentAlignment.MiddleCenter;
-
-    leftButton.Text = "OK";
+    // Set up locations
+    redCircle.X = (Width-redCircle.Width-text.Width)/2;
+    text.Location = new Point(redCircle.Right, text.Height);
+    redCircle.Y = text.Top-(redCircle.Height-text.Height)/2;
     leftButton.Location = new Point((Width - 2*leftButton.Width)/3, Height/2);
-    // leftButton.Enabled = false;  //disabling this button is optional
-
-    rightButton.Text = "Accept";
     rightButton.Location = new Point(2*leftButton.Left+rightButton.Width, leftButton.Top);
-    // rightButton.Enabled = false; //disabling this button is also optional
+
+    // Optional stuff
+    // leftButton.Enabled = false;  //prevents the left button from being clicked on
+    // rightButton.Enabled = false; //prevents the right button from being clicked on
 
     // Add the created Controls to the Form
     Controls.Add(text);
     Controls.Add(leftButton);
     Controls.Add(rightButton);
-    
+
     // Tell the events which method to call (The method is defined below)
     leftButton.Click += new EventHandler(exit);
     rightButton.Click += new EventHandler(exit);
   }
-  
+
+  protected override void OnPaint(PaintEventArgs e)
+  {
+    Graphics graphics = e.Graphics;
+
+    graphics.FillEllipse(Brushes.Red, redCircle);
+
+    base.OnPaint(e);
+  }
+
   protected void exit(Object sender, EventArgs events)
   {
     System.Console.WriteLine("You clicked on the Exit button. This program will now end.");
